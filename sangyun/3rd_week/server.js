e4a28fc2a789dbf6715a41ad4e164eaf42f5e962
sangyun lee <sororiri@gmail.com>
2022-10-16 01:32:28 +0900

    Add : [Project] JUSTGRAM - Node Mission 4 (POST CRUD) 완료 - POST CRUD API 완료
    - ref : https://www.notion.so/wecode/Project-JUSTGRAM-Node-99706f133dbd4c788b548cb7d4c5f00a

--- sangyun/3rd_week/server.js
+++ sangyun/3rd_week/server.js
@@ -32,10 +32,19 @@ dataSource.initialize().then(() => {
 });
 
 
-
+// TODO: 좋아요 API 만들어보기
 
 // user route
 app.post('/user', addUser);
+
+// posting route
+app.post('/post', addPost);
+app.get('/post', getPostAll);
+app.patch('/post/:id', patchPost);
+app.delete('/post/:id', deletePost);
+app.get('/post/user/:id', getPostByUserId);
+
+
 
 // register user
 async function addUser(req, res) {
@@ -70,9 +79,235 @@ async function addUser(req, res) {
   }
 };
 
+async function addPost(req, res) {
+  const { contents, image_url, user_id} = req.body;
+  if (!contents | !image_url) {
+    res.status(400).json({ message: "plz fill out 'contents, image_url'" });
+    return;
+  }
+
+  const answer = await dataSource.transaction(async (transactionalEntityManager) => {
+    // execute queries using transactionalEntityManager
+    const posting = await transactionalEntityManager.query(
+      `INSERT INTO postings(
+                          contents,
+                          user_id
+                        ) VALUES (?, ?);
+                        `,
+      [contents, user_id]
+    );
+    await transactionalEntityManager.query(
+      `INSERT INTO posting_images(
+                        posting_id,
+                        image_url
+                        ) VALUES (?, ?);
+                        `,
+      [posting.insertId, image_url]
+    );
+    return posting;
+    })
+  .catch(e => {
+    console.log(e.sqlMessage);
+    return Promise.resolve(undefined);
+  });
+
+  if (answer) {
+    res.status(201).json({ message: "successfully created" });
+  } else {
+    res.status(500).json({ message: "failed to create" });
+  }
+}
+
+async function getPostAll(req, res) {
+  const answer = await dataSource
+    .query(
+      `SELECT
+        users.id AS userId,
+        users.profile_image AS userProfileImage,
+        postings.id AS postingId,
+        posting_images.image_url AS postingImageUrl,
+        postings.contents AS postingContent
+
+        FROM postings
+        JOIN users ON postings.user_id = users.id
+        JOIN posting_images ON posting_images.posting_id = postings.id
+      `)
+    .catch((err) => {
+      console.log(err);
+      return Promise.resolve(undefined);
+    });
+
+  console.dir(answer);
+
+  if (answer) {
+    res.status(201).json({ data: answer });
+  } else {
+    res.status(500).json({ message: "failed to create" });
+  }
+}
+
+async function getPostByUserId(req, res) {
+  const userId = req.params.id;
+  const answer = await dataSource
+    .query(
+      `SELECT
+        users.id AS userId,
+        users.profile_image AS userProfileImage,
+        postings.id AS postingId,
+        posting_images.image_url AS postingImageUrl,
+        postings.contents AS postingContent
+
+        FROM postings
+        JOIN users ON postings.user_id = users.id
+        JOIN posting_images ON posting_images.posting_id = postings.id
+
+        WHERE users.id = ?
+      `,
+      [userId])
+    .then((answer) => {
+      if (!answer) {
+        return Promise.resolve(answer);
+      }
+      return Promise.resolve({
+        userId: answer[0].userId,
+        userProfileImage: answer[0].userProfileImage,
+        postings: [...answer].map((post) => {
+          return {
+            postingId : post.postingId,
+            postingImageUrl: post.postingImageUrl,
+            postingContent: post.postingContent
+          }
+        })
+        }
+      );
+    })
+    .catch((err) => {
+      console.log(err);
+      return Promise.resolve(undefined);
+    });
+
+  if (answer) {
+    res.status(201).json({ data: answer });
+  } else {
+    res.status(500).json({ message: "failed" });
+  }
+}
+
+async function patchPost(req, res) {
+  const postId = req.params.id;
+  const { contents, image_url } = req.body;
+  if (!contents | !image_url) {
+    res.status(400).json({ message: "plz fill out 'contents, image_url'" });
+    return;
+  }
+
+  const answer = await dataSource.transaction(
+    async (transactionalEntityManager) => {
+      // patch contents
+      if (contents) {
+        await transactionalEntityManager.query(
+          `UPDATE postings
+              SET contents = ?
+            WHERE postings.id = ?
+          `,
+          [contents, postId]
+        );
+      }
+      // patch image_url
+      if (image_url) {
+          // 먼저 기존 image_url을 전부 지움
+        await transactionalEntityManager.query(
+          `DELETE FROM posting_images
+            WHERE posting_images.posting_id = ?;`
+          ,[postId]);
+
+          // 이후 image_url을 업데이트함.
+        await transactionalEntityManager.query(
+          `INSERT INTO posting_images(
+            posting_id,
+            image_url
+          ) VALUES (?, ?);`
+          ,[postId, image_url]);
+      }
+      return await getPost(postId);
+  })
+  // transaction 실패
+  .catch(e => {
+    console.log(e);
+    return Promise.resolve(undefined);
+  });
+
+  if (answer) {
+    res.status(200).json({data: answer});
+  } else {
+    res.status(500).json({ message: "failed to patch" });
+  }
+}
+
+async function deletePost(req, res) {
+  const postId = req.params.id;
+
+  const answer = await dataSource.transaction(
+    async (transactionalEntityManager) => {
+      await transactionalEntityManager.query(
+        `DELETE FROM posting_images
+        WHERE posting_images.posting_id = ?;`
+      ,[postId])
+
+      const result = await transactionalEntityManager.query(
+        `DELETE FROM postings
+        WHERE postings.id = ?;`
+      ,[postId])
+
+      if (!result.affectedRows) {
+        return Promise.reject({status: 404, message: "postID에 해당하는 포스트가 존재하지 않습니다"});
+      }
+
+      return Promise.resolve({status: 200, message: "successfully deleted"});
+  })
+  .catch(err => {
+    if (err.sqlMessage) {
+      return Promise.resolve({status: 500, message: "failed"});
+    }
+    return Promise.resolve(err);
+  });
+
+  res.status(answer.status).json({ message: answer.message });
+}
+
+async function getPost(postId) {
+  /*
+  아래와 같은 값을 반환함.
+  {
+    "userId": 9,
+    "userName": "피리",
+    "postingId": 56,
+    "postingImageUrl": "PATCH123.jpg",
+    "postingContent": "patchTEST123"
+  }
+  */
+  return await dataSource
+    .query(
+      `SELECT
+        users.id AS userId,
+        users.nickname AS userName,
+        postings.id AS postingId,
+        posting_images.image_url AS postingImageUrl,
+        postings.contents AS postingContent
+
+        FROM postings
+        JOIN users ON postings.user_id = users.id
+        JOIN posting_images ON posting_images.posting_id = postings.id
+
+        WHERE postings.id = ?
+      `,
+    [postId]);
+}
 
 // init
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+
