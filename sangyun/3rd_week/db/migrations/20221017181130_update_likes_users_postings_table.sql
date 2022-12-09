8d8e678d23f531590b0ccfe27eae5aa92f7c354d
sangyun lee <sororiri@gmail.com>
2022-12-09 20:02:36 +0900

    [justcode 이상윤] fastfive 프로젝트 DB로 변경 - fastfive 프로젝트 ERD에 맞게 DBmate 파일 변경

--- sangyun/3rd_week/db/migrations/20221017181130_update_likes_users_postings_table.sql
+++
@@ -1,5 +0,0 @@
--- migrate:up
-ALTER TABLE likes_postings_users ADD likes boolean NOT NULL;
-
--- migrate:down
-ALTER TABLE likes_postings_users DROP COLUMN likes;
--- sangyun/3rd_week/db/migrations/20221017190318_update_likes_users_postings_table.sql
+++
@@ -1,5 +0,0 @@
--- migrate:up
-ALTER TABLE likes_postings_users DROP COLUMN likes;
-
--- migrate:down
-ALTER TABLE likes_postings_users ADD likes boolean NOT NULL;
