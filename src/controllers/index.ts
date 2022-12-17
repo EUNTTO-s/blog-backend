1da09264c9685252ba375b6ca375a0528e17aba4
kihyeonKim <kidone0033@gmail.com>
2022-12-17 17:55:28 +0900

    feature/category CRUD API 작성

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,11 +1,15 @@
 import cmtCtl from "./comment.controller";
 import userCtl from "./user.controller";
 import postCtl from "./post.controller";
+import cateCtl from "./category.controller";
+import locationCtl from "./location.controller";
 
 const ctl_set = {
     cmtCtl,
     postCtl,
     userCtl,
+    cateCtl,
+    locationCtl
 };
 
 export default ctl_set;
--- src/services/index.ts
+++ src/services/index.ts
@@ -2,12 +2,14 @@ import cmtSvc from "./comment.service";
 import userSvc from "./user.service";
 import user_Svc from "./user_.service";
 import postSvc from "./post.service";
+import cateSvc from "./category.service";
 
 const svc_set = {
     cmtSvc,
     postSvc,
     userSvc,
     user_Svc,
+    cateSvc
 };
 
 export default svc_set;
