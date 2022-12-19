804d2bc6b85e72956e1aa6778d2a2f9a0aa3c50e
kihyeonKim <kidone0033@gmail.com>
2022-12-20 00:38:48 +0900

    location 1차..

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,11 +1,13 @@
 import cmtCtl from "./comment.controller";
 import userCtl from "./user.controller";
 import postCtl from "./post.controller";
+import locationCtl from "./location.controller";
 
 const ctl_set = {
     cmtCtl,
     postCtl,
     userCtl,
+    locationCtl,
 };
 
 export default ctl_set;
