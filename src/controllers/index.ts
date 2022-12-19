7da473d55b04c8a14a2c8ca3a91bda1484af9733
kihyeonKim <kidone0033@gmail.com>
2022-12-20 00:28:39 +0900

    1차 이후 바로 2차 category...

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,14 +1,14 @@
 import cmtCtl from "./comment.controller";
 import userCtl from "./user.controller";
 import postCtl from "./post.controller";
-// import cateCtl from "./category.controller";
+import cateCtl from "./category.controller";
 // import locationCtl from "./location.controller";
 
 const ctl_set = {
     cmtCtl,
     postCtl,
     userCtl,
-//     cateCtl,
+    cateCtl,
 //     locationCtl
 };
 
