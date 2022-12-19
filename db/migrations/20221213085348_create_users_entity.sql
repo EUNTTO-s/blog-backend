8862ebe42d3e7549d892d2f85bef56eaf75464fb
sangyun lee <sororiri@gmail.com>
2022-12-19 17:08:43 +0900

    Add: DB에 유저 계정 추가 및 멤버 그룹 추가

--- db/migrations/20221213085348_create_users_entity.sql
+++ db/migrations/20221213085348_create_users_entity.sql
@@ -1,7 +1,13 @@
 -- migrate:up
 INSERT INTO
 users (id, username, email, password)
-VALUES (1, 'admin', 'admin@gmail.com', '$2a$10$pxMSg9pMZ8yQjY/o3hIFnuRCk5PDnWtVYVeXkqGbws6eXFuC9VxVK');
+VALUES
+  (1, 'admin', 'admin@gmail.com', '$2a$10$pxMSg9pMZ8yQjY/o3hIFnuRCk5PDnWtVYVeXkqGbws6eXFuC9VxVK'),
+  (2, 'test1', 'test1@naver.com', '$2a$10$A38j9gaM4VALxKfMNVroRu.h1tvg8Yoi02oaHHA5WJNpvRLG3UqI6'), -- pwd: test123@
+  (3, 'test2', 'test2@naver.com', '$2a$10$A38j9gaM4VALxKfMNVroRu.h1tvg8Yoi02oaHHA5WJNpvRLG3UqI6'),
+  (4, 'test3', 'test3@naver.com', '$2a$10$A38j9gaM4VALxKfMNVroRu.h1tvg8Yoi02oaHHA5WJNpvRLG3UqI6'),
+  (5, 'test4', 'test4@naver.com', '$2a$10$A38j9gaM4VALxKfMNVroRu.h1tvg8Yoi02oaHHA5WJNpvRLG3UqI6');
+
 
 INSERT INTO
 admins (users_id)
