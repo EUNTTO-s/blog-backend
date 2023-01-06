7c8b527b70b909e18bdf260adfaa64ab5a336882
sangyun lee <sororiri@gmail.com>
2023-01-06 13:44:29 +0900

    Fix: 사용되지 않는 코드 제거

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,8 +1,6 @@
-import companyCtl from "./company.controller";
 import userCtl from "./user.controller";
 
 const ctl_set = {
-    companyCtl,
     userCtl,
 };
 
--- src/routers/company.router.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import companyCtl from "../controllers/company.controller";
-import { asyncWrap } from "../utils/myutils";
-const router = express.Router();
-
-// routing
-router.get("/company", asyncWrap(companyCtl.getCompanies));
-
-export default router;
