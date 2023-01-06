e99ea07b6a43b42cb4eb892a79d4d7b35b08c6d1
sangyun lee <sororiri@gmail.com>
2023-01-06 20:58:47 +0900

    Fix: rest api URL 명명 규칙 적용 - 특정 그룹의 아이템을 명시할 때에는 그룹명(categories) 뒤에 id를 적도록함.   ex) www.sample.com/categories/1 - order by 적용

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -6,26 +6,27 @@ import {checkDataIsNotEmpty,} from '../utils/myutils'
 const createCategories = async (req: express.Request, res: express.Response) => {
   const { categoryName } = req.body;
   await cateSvc.createCategories(req.userInfo.id, categoryName);
-  res.status(200).json('CATEGORY_CREATED');
+  res.status(201).json({ message: "CATEGORY_CREATED" });
 }
 
 const getCategories = async (req: express.Request, res: express.Response) => {
-  const {userId} = req.params;
+  const { id: userId } = req.params;
   const categories = await cateSvc.getCategories({userId});
   res.status(200).json(categories);
 }
 
 const updateCategories = async (req: express.Request, res: express.Response) => {
-  const { categoryName, categoryId } = req.body;
+  const { id: categoryId } = req.params;
+  const { categoryName } = req.body;
   await cateSvc.updateCategories(req.userInfo.id, categoryId, categoryName);
-  res.status(200).json('CATEGORY_UPDATED');
+  res.status(200).json({ message: "CATEGORY_UPDATED" });
 }
 
 const deleteCategories = async (req: express.Request, res: express.Response) => {
-  const { categoryId } = req.body;
+  const { id: categoryId } = req.params;
   checkDataIsNotEmpty({categoryId});
   await cateSvc.deleteCategories(req.userInfo.id, categoryId);
-  res.status(200).json('CATEGORY_DELETED');
+  res.status(200).json({ message: "CATEGORY_DELETED" });
 }
 
 export default {
