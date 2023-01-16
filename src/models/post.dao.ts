c4f7cd6dd4b41a47bbdc4abfe072a91cc17c9111
sangyun lee <sororiri@gmail.com>
2023-01-16 18:26:08 +0900

    refactor: sql 쿼리를 다른 ts폴더로 이동시켜 가독성을 향상시킴

--- src/models/post.dao.ts
+++ src/models/post.dao.ts
@@ -1,4 +1,9 @@
 import dataSource from "./database";
+import {
+  getQueryOfSelectPost,
+  getQueryOfOpenRange,
+  getQueryOfMyFollow,
+} from "./builder/post.sql";
 import { whereBuilder, setBuilder } from "./builder/queryBuilder";
 
 const createPosts = async (postInput: PostInputType) => {
@@ -47,95 +52,13 @@ const getPosts = async (searchOption: PostSearchOption) => {
   }
   const answer = await dataSource.query(
       `
-      SELECT
-        p.id,
-        p.title,
-        p.content,
-        p.thumnail_img_url AS thumnailImgUrl,
-        p.secret_type AS secretType,
-        p.created_at as createdAt,
-        JSON_OBJECT(
-          'id',
-          cate.id,
-          'categoryName',
-          cate.category_name
-        ) AS category,
-        JSON_OBJECT(
-          'id',
-          u.id,
-          'nickname',
-          u.nickname
-        ) AS user,
-        JSON_OBJECT(
-          'id',
-          t.id,
-          'topicName',
-          t.topic_name
-        ) AS topic,
-        tagsOnPost.tags
-      FROM
-        posts AS p
-        JOIN users AS u
-          ON u.id = p.users_id
-        LEFT JOIN categories AS cate
-          ON cate.id = p.categories_id
-        LEFT JOIN topics AS t
-          ON t.id = p.topics_id
-        LEFT JOIN ( -- select list of tag on post
-          SELECT
-            posts_id,
-            JSON_ARRAYAGG(JSON_OBJECT("id", tags_id, "tagName", tag_name)) AS tags
-          FROM
-            posts_tags pt
-            JOIN tags t ON t.id = pt.tags_id
-          GROUP BY
-            posts_id
-        ) tagsOnPost
-          ON tagsOnPost.posts_id = p.id
+      ${getQueryOfSelectPost()}
       ${whereBuilder("p.id",        ["="], postId, true)}
       ${whereBuilder("p.users_id",  ["="], userId)}
       ${whereBuilder("cate.id",     ["="], categoryId)}
       ${whereBuilder("t.id",        ["="], topicId)}
-      AND
-        (
-          p.secret_type = 0
-          ${loginedUserId?
-          ` -- 당신꺼면 볼 수 있다.
-            OR (p.users_id = ?)
-            -- 맞팔이면 볼 수 있다.
-            OR
-              (
-                p.secret_type = 1 AND p.users_id IN
-                (
-                  SELECT DISTINCT
-                    myFollow.target_users_id AS users_id
-                  FROM
-                    follow myFollow,
-                    follow yourFollow
-                  WHERE
-                  (
-                    myFollow.users_id = ?
-                    AND myFollow.users_id = yourFollow.target_users_id
-                    AND myFollow.target_users_id = yourFollow.users_id
-                  )
-                )
-              )
-          ` : ""}
-        )
-      ${myFollowing ?
-        `
-        AND p.users_id IN
-        (
-          SELECT DISTINCT
-            follow.target_users_id AS users_id
-          FROM
-            follow
-          WHERE
-          (
-            follow.users_id = ?
-          )
-        )
-        `: ''}
+      ${getQueryOfOpenRange(loginedUserId)}
+      ${getQueryOfMyFollow(myFollowing)}
       ${whereBuilder("p.title",             ["LIKE", "AND", "SEARCH"], search)}
       ${whereBuilder("p.content",           ["LIKE", "OR",  "SEARCH"], search)}
       ${whereBuilder("cate.category_name",  ["LIKE", "OR",  "SEARCH"], search)}
