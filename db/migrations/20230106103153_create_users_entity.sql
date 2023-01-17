29164ee25b7142a509c6e68269d81afe037fa872
sangyun lee <sororiri@gmail.com>
2023-01-17 10:35:19 +0900

    Fix: mock DB 추가 - 원인: 잦은 DB 롤백으로 DB 초기화가 많이 이루어져 테스트가 불편함. - 해결: mock DB 를 생성하여 초기화될 때에 유저와 팔로우리스트가 DB에 미리 추가될 수 있도록 함.

--- db/migrations/20230106103153_create_users_entity.sql
+++ db/migrations/20230106103153_create_users_entity.sql
@@ -3,8 +3,33 @@ INSERT INTO
   users (nickname, email, password)
 VALUES
   (
-    '테스트1',
+    '테스터',
     'test1@gmail.com',
+    '$2a$10$2zJdcIpvQ5FFX.SZQKQq9edBSYmrl8Gk/uAtNSEJeceHH0xXZIWHS'
+  ), -- test123@
+  (
+    '짱구',
+    'test2@gmail.com',
+    '$2a$10$2zJdcIpvQ5FFX.SZQKQq9edBSYmrl8Gk/uAtNSEJeceHH0xXZIWHS'
+  ), -- test123@
+  (
+    '액션가면',
+    'test3@gmail.com',
+    '$2a$10$2zJdcIpvQ5FFX.SZQKQq9edBSYmrl8Gk/uAtNSEJeceHH0xXZIWHS'
+  ), -- test123@
+  (
+    '봉미선',
+    'test4@gmail.com',
+    '$2a$10$2zJdcIpvQ5FFX.SZQKQq9edBSYmrl8Gk/uAtNSEJeceHH0xXZIWHS'
+  ), -- test123@
+  (
+    '철수',
+    'test5@gmail.com',
+    '$2a$10$2zJdcIpvQ5FFX.SZQKQq9edBSYmrl8Gk/uAtNSEJeceHH0xXZIWHS'
+  ), -- test123@
+  (
+    '신짱아',
+    'test6@gmail.com',
     '$2a$10$2zJdcIpvQ5FFX.SZQKQq9edBSYmrl8Gk/uAtNSEJeceHH0xXZIWHS'
   ) -- test123@
 
