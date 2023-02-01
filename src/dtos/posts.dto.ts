19c92d3b30d8a782fe3e87dc60cf0b742e38ce49
sangyun lee <sororiri@gmail.com>
2023-02-01 13:03:59 +0900

    Refactor: dto 검증을 위한 데코레이터 적용

--- src/dtos/posts.dto.ts
+++ src/dtos/posts.dto.ts
@@ -2,18 +2,40 @@ import express from 'express';
 import {checkDataIsNotEmpty,} from '../utils/myutils'
 import { OpenRange } from "../types/post.types";
 import { enumToArray } from "../utils/myutils";
-
+import {
+  MinLength,
+  MaxLength,
+  IsNumber,
+  ArrayMaxSize,
+  validate,
+  IsIn,
+} from 'class-validator';
 export class CreatePostDto {
   postId?: number;
+
+  @MinLength(1, {message: '제목은 최소 1글자 이상이여야 합니다',})
+  @MaxLength(300, {message: '제목은 최대 300글자 이하이여야 합니다',})
   title?: string;
+
+  @IsNumber()
   userId?: number;
+
   cateId?: number;
+
+  @MinLength(1, {message: '내용은 최소 1글자 이상이여야 합니다.',})
+  @MaxLength(1500, {message: '내용은 최대 1500글자 이하이여야 합니다',})
   content?: string;
+
   thumnail?: Express.Multer.File;
   thumbnailImgUrl?: string;
+
+  @IsIn(enumToArray(OpenRange), {message: "secretType가 0~2 사이에 존재하지 않습니다. 0: 전체공개, 1: 맞팔공개, 2: 비공개"})
   secretType?: number;
   topicId?: number;
+
+  @ArrayMaxSize(10, {message: '태그는 최대 10개까지 넣을 수 있습니다',})
   tagNames?: string[];
+
   loginedUserId?: number;
 
   constructor({title, userId, categoryId, content, thumnail, secretType, topicId, tagNames, postId, loginedUserId}: any) {
@@ -28,30 +50,9 @@ export class CreatePostDto {
     this.topicId = topicId && +topicId;
     this.tagNames = tagNames?.split(',') ?? [];
     this.loginedUserId = loginedUserId && +loginedUserId;
-
-    // title 길이 검사
-    if (this.title.length > 300) {
-      throw { status: 400, message: "제목은 300자 이상 적을 수 없습니다." };
-    }
-
-    // content 길이 검사
-    if (this.content.length > 1500) {
-      throw { status: 400, message: "내용은 1500자 이상 적을 수 없습니다." };
-    }
-
-    // 태그 개수 제한 검사
-    if (tagNames.length > 10) {
-      throw { status: 400, message: "태그는 10개를 초과할 수 없습니다." };
-    }
-
-    // 공개타입이 범위 내에 존재하는 지 확인
-    const inRange = enumToArray(OpenRange).includes(Number(secretType));
-    if (!inRange) {
-      throw { status: 400, message: "secretType가 0~2 사이에 존재하지 않습니다. 0: 전체공개, 1: 맞팔공개, 2: 비공개" };
-    }
   }
 
-  static factory(req: express.Request) : CreatePostDto {
+  static async factory(req: express.Request) : Promise<CreatePostDto> {
     const input = {
       ...req.body,
       postId: req.params.postId,
@@ -59,7 +60,13 @@ export class CreatePostDto {
       cateId: req.body.categoryId,
       thumnail: req.body.thumnail === undefined ? req.file : "",
     }
-    return new CreatePostDto(input);
+    const dto = new CreatePostDto(input);
+    await validate(dto, {skipMissingProperties: true, validationError: { target: false }}).then(errors => {
+      if (errors.length > 0) {
+        throw { status: 400, message: errors[0].constraints };
+      }
+    });
+    return dto;
   }
 }
 
